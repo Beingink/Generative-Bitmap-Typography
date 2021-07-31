b40ce3ca234c51407d858ab1df959ec2472857e3
Indhu Kanth <indhukanthl@gmail.com>
2021-07-31 23:39:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ frameRate(25);
     
   
 
-let g = createP('D I S S O C I A T I V E');
+let g = createP('D I S S O S C I A T I V E');
 g.style('font-size', '30px');
 g.position(80, 12);
 g.style('color', '#5500cc');
